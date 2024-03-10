7e96a02457d2dd439975992d4f34c18cf27fce82
Shivanshu33000 <161638613+Shivanshu33000@users.noreply.github.com>
2024-03-10 23:30:12 +0530

    Add files via upload

---
+++ vector sorting code.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>a;
+    int x , n;
+    cout<< "Enter The Size of Array:-  ";
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        cin >> x;
+        a.push_back(x);
+    } sort(a.begin(),a.end());
+    for(int i = 0 ; i < n ; i++){
+        cout << a[i];
+    }
+    
+}
